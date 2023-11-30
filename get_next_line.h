0fbf86895223ef9a458401d38d0c2843a56f851e
ChrisAjin <103993693+ChrisAjin@users.noreply.github.com>
2023-11-30 12:56:37 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -10,25 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
+ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <fcntl.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <strings.h>
+
 # include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 4000
-# endif
+# define READL_OK 1
+# define EOF_RCHD 0
+# define ERR_HPND -1
 
-char	*read_files(int rd, char *rest, int fd, char *buffer);
-char	*get_next_line(char *rest);
-char	*get_new_rest(char *rest);
-char	*get_next_line(int fd);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlen(const char *str);
+# define NO_ENDLINE 0
+# define FOUND_ENDLINE 1
+
+int		get_next_line(int fd, char **line);
+
+/*
+** Utils
+*/
+
+size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char *s1, char const *s2);
+
 #endif
